41965f9909a49f8c008a37ec8537f3ec8d47543b
Ibil <cyber_man_7@hotmail.com>
2022-09-06 23:45:01 +0100

    fix call

--- src/app/services/GridItems.ts
+++ src/app/services/GridItems.ts
@@ -1,7 +1,7 @@
 
 export const getProducts = async () => {
     const result = await fetch("https://api-store-indielisboa.herokuapp.com/v1/products?limit=100&page=0&stock=false");
-    console.log(result.json);
+    console.log(result.json());
 }
 
 const deleteProduct = () => {
