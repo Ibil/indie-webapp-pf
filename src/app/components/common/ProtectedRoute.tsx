172c99db3ee6bc636cc465a4da2967bc00b17069
Ibil <cyber_man_7@hotmail.com>
2022-09-08 22:31:52 +0100

    redirect to login

--- src/app/components/common/ProtectedRoute.tsx
+++ src/app/components/common/ProtectedRoute.tsx
@@ -7,22 +7,13 @@ export interface ProtectedRouteProps extends RouteProps {
   isAuthenticated: boolean;
 }
 
-const differentRoute = (path1: string, path2: string) : boolean => path1 !== path2; 
-
 export const ProtectedRoute: React.FC<ProtectedRouteProps> = props => {
   const currentLocation = useLocation();
-  const authenticationPathValue = props.authenticationPath ? props.authenticationPath : "/login";
-  let redirectPath = currentLocation.pathname;
 
   if (!props.isAuthenticated) {
-    const suffix = currentLocation.pathname === "/" ? "" : "/" + encodeURIComponent(currentLocation.pathname);
-    redirectPath = authenticationPathValue + suffix;
-  }
-
-  if (differentRoute(redirectPath, currentLocation.pathname)) {
-    const renderComponent = () => <Redirect to={{ pathname: redirectPath }} />;
-    return <Route {...props} component={renderComponent} render={undefined} />;
-  } else {
+    const renderComponent = () => <Redirect to={{ pathname: "/login", state: { from: currentLocation.pathname } }} />;
+    return <Route {...props} component={renderComponent} render={undefined} />
+  }else {
     return <Route {...props} />;
   }
 };
